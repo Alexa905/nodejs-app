b0b5f3193ea0058569fed5b525db4a38a3f2af1b
Alexeenkova Tatyana <alexeenkovatanya@gmail.com>
2017-11-14 23:50:00 +0100

    homework4

--- app.js
+++ app.js
@@ -1,28 +1,19 @@
-import * as configs from './config/config.json'
-import { User, Product }  from './models'
-import { DirWatcher, Importer }  from './modules'
-import EventEmitter from 'events'
-const csvPath = 'data/data.csv';
-
-const emitter =  new EventEmitter();
-const eventName = 'dirwatcher:changed';
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+import { cookieParser, queryParser, errorHandler} from './middlewares';
+import { products, users } from './routes';
 
 
-console.log(`Application name: ${configs['app-name']}`);
+app.use(errorHandler);
+app.use(cookieParser);
+app.use(queryParser);
+app.use(bodyParser.json());
 
-const user = new User();
-const product = new Product();
+app.use('/api/users', users);
+app.use('/api/products', products);
 
-const dirWather = new DirWatcher(emitter, eventName);
-const importer = new Importer(emitter, eventName);
-
-dirWather.watch('./data', 3000);
-importer.importAsync(csvPath).then(data =>{
-	console.log('Imported async data: \n', data);
-});
-
-const importedData = importer.importSync(csvPath);
-console.log('Imported async data: \n', importedData);
+module.exports = app;
 
 
 
